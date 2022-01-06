5c591c1284fb64c3d0429e136a896dbf3afa9063
tobywinn <63807563+tobywinn@users.noreply.github.com>
2022-01-06 11:51:22 +0000

    pdf scraper and annotation

---
+++ project/Scrapers/arkesdenscraper.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+# // Web scraping: 
+import tabula
+from bs4 import BeautifulSoup
+#Installing PDF reader 
+#import camelot 
+
+file1 = "file:///Users/tobywinn/Downloads/Arkesden%20Banking%20Compensation%20Report%202020-2021.pdf"
