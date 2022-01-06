b0debfb43a424883ff8bd41cf16f587d4854c560
tobywinn <63807563+tobywinn@users.noreply.github.com>
2022-01-06 14:08:46 +0000

    new arkesden data set

--- project/Scrapers/arkesdenscraper.py
+++ project/Scrapers/arkesdenscraper.py
@@ -1,9 +1,38 @@
-import numpy as np
-import pandas as pd
-# // Web scraping: 
 import tabula
-from bs4 import BeautifulSoup
-#Installing PDF reader 
-#import camelot 
+import pandas as pd
 
-file1 = "file:///Users/tobywinn/Downloads/Arkesden%20Banking%20Compensation%20Report%202020-2021.pdf"
+# Read remote pdf into a list of DataFrame
+df = tabula.convert_into("project/Arkesden Banking Compensation Report 2020-2021.pdf", "project/ArkesdenData.csv", output_format='csv', lattice=True, pages=4)
+
+df = pd.read_csv("project/ArkesdenData.csv")
+
+# cant replace all in this case as yields bug due to * character
+df['LEVEL'] = df['LEVEL'].str.replace("*", "")
+
+df['LEVEL'] = df['LEVEL'].str.replace("\r", " ")
+df = df.replace('£','', regex=True)
+
+df['Analyst 3'] = df['Analyst 3'].str.replace("*", "", regex=False)
+
+# get header titels which we will want at end
+seniority = df.columns.tolist()
+del seniority[10:]
+seniority[0] = "Firm"
+
+# make years new header
+new_header = df.iloc[0] #grab the first row for the header
+df = df[1:] #take the data less the header row
+df.columns = new_header #set the header row as the df header
+
+# drop previous years values according to header value
+df = df.drop('19-20', 1)
+
+# rename header as we desire
+df.columns = seniority
+
+# format numbers so no k or K
+df = df.replace('K','', regex=True)
+df = df.replace('k','', regex=True)
+
+# save
+df.to_csv('project/ArkesdenData.csv')
